a66f04b0c7fc8de05e455c3a4e60daa17c2d22b2
Dexter Urbano <dexterurbano25@gmail.com>
2024-09-24 21:53:11 +0800

    add sign in page

--- user/views.py
+++ user/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def sign_up(request):
     return render(request,'user/sign-up.html')
+
+def sign_in(request):
+    return render(request,'user/sign-in.html')
