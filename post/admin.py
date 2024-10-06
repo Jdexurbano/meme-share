4ac6d7b1f53714b4a51055815ee8a39436926a94
Dexter <udexter324@gmail.com>
2024-10-06 17:27:11 +0800

    edit some changes to admin.py file

--- post/admin.py
+++ post/admin.py
@@ -6,4 +6,4 @@ class PostAdmin(admin.ModelAdmin):
 
 
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
--- user/admin.py
+++ user/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user')
+    list_display = ('user','profile_link')
 
 # Register your models here.
 admin.site.register(Profile,ProfileAdmin)
