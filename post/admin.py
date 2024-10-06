6be9a456c353ebb1e48350cfae9350f9e71ddbfc
Dexter <udexter324@gmail.com>
2024-10-06 16:38:23 +0800

    add Post model

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('caption','created_at','updated_at')
+
 
 # Register your models here.
+admin.site.register(Post)
--- post/models.py
+++ post/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    caption = models.CharField(max_length=300,blank=True)
+    image_link = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    #primary key from the user
+    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='posts')
+
+
+    def __str__(self):
+        return self.caption
