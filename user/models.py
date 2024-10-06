b53188ba4c877a315eb9f9a1b510f6a87c67df8b
Dexter <udexter324@gmail.com>
2024-10-06 17:22:08 +0800

    add  profile custom field to the user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+#custom user field
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    profile_link = models.CharField(max_length=300)
+
+    def __str__(self):
+        return str(self.user)
