d92903e9384d5732f0efa5241ac993de293b91ca
Dexter <udexter324@gmail.com>
2024-10-06 16:59:42 +0800

    add functionality to add a post

--- post/views.py
+++ post/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Post
 
 # Create your views here.
 def add_post(request):
+    if request.method == 'POST':
+        caption = request.POST['caption']
+        image_link = request.POST['image_link']
+        #get the current login user
+        user = request.user
+
+        #save the post to the database
+        Post.objects.create(caption = caption, image_link = image_link, user = user)
+
+        return redirect('home')
+    
     return render(request,'post/add_post.html')
