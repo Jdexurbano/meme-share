43ebf2525977881a8a3365ebae482b9704b40cf4
Dexter <udexter324@gmail.com>
2024-10-06 17:08:37 +0800

    display all the post from the database

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from post.models import Post
 # Create your views here.
 
 
@@ -9,4 +10,9 @@ def index(request):
 
 @login_required
 def home_page(request):
-    return render(request,'feed/home.html')
+    posts = Post.objects.all().order_by('-created_at')
+
+    context = {
+        'posts':posts
+    }
+    return render(request,'feed/home.html',context)
